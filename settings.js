fc0dd3952ea45958e9898768815c2bcc7ac442a9
Viperxr226 <uzezijenniferadeniyi@gmail.com>
2024-10-11 12:00:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,9 +8,9 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '2348022142390'  //creator number
+global.ownername = 'VIPER LORD👑' //owner name
+global.botname = 'VIPER ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
